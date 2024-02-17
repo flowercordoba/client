4e66c9eb9144b42ee89d638b1e6281e9f6d66bbc
flowercordoba <flowercordoba7@gmail.com>
2024-02-16 19:02:36 -0500

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './index.scss'
+
+export  function App() {
+  return (
+    <h1 className="text-3xl font-bold underline bg-slate-200">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
